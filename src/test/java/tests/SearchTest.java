1068d3e3d389244489411ecb24a84f27cdba77de
Nikita Rysiev <nickpolska21@gmail.com>
2020-12-18 21:42:23 +0100

    Added: - more tests - page factory - explicit wait

--- src/test/java/tests/SearchTest.java
+++ src/test/java/tests/SearchTest.java
@@ -10,15 +10,11 @@ public class SearchTest extends BaseTest {
     @Test
     void shouldReturnCorrectProductListWhenPositiveSearchPhraseIsUsed() {
         HomePage homePage = new HomePage(driver, wait);
-
         homePage.openPage();
-
         homePage.searchForProduct("dress");
 
         SearchResultPage searchResultPage = new SearchResultPage(driver, wait);
-
         Assertions.assertTrue(searchResultPage.isProductWithNameVisible("dress"));
-
         Assertions.assertEquals("7 results have been found.", searchResultPage.getSearchSummary());
     }
 }
